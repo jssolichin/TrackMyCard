d68749e2e36ad43e20fd5ae4c3d9640dd9550bbe
Jonathan Solichin <jssolichin@users.noreply.github.com>
2026-02-01 23:11:02 -0800

    Initial Commit

---
+++ TrackMyCard/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TrackMyCard
+//
+//  Created by Jonathan Solichin on 2/1/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TrackMyCardTests/TrackMyCardTests.swift
@@ -0,0 +1,16 @@
+//
+//  TrackMyCardTests.swift
+//  TrackMyCardTests
+//
+//  Created by Jonathan Solichin on 2/1/26.
+//
+
+import Testing
+
+struct TrackMyCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
